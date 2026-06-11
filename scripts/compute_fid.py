0b39240ddab914379a9b6d1fdb2e728aaeb4aab6
king26original <analogsns@gmail.com>
2026-06-11 18:47:05 +0530

    Update compute_fid.py

--- scripts/compute_fid.py
+++ scripts/compute_fid.py
@@ -1,6 +1,8 @@
 from src.evaluation.fid import fid, fid_fn
+from src.diffusion.cosine_beta_schedule import cosine_alpha, cosine_alpha_bar, cosine_posterior_variance
+from src.diffusion.linear_beta_schedule import linear_alpha, linear_alpha_bar, linear_posterior_variance
 
-fid_score_model1=fid_fn(model1, fid)
-fid_score_model2=fid_fn(model2, fid)
-fid_score_model3=fid_fn(model3, fid)
-fid_score_model4=fid_fn(model4, fid)
+fid_score_model1=fid_fn(model1, fid, linear_alpha, linear_alpha_bar, linear_posterior_variance)
+fid_score_model2=fid_fn(model2, fid, linear_alpha, linear_alpha_bar, linear_posterior_variance)
+fid_score_model3=fid_fn(model3, fid, cosine_alpha, cosine_alpha_bar, cosine_posterior_variance)
+fid_score_model4=fid_fn(model4, fid, cosine_alpha, cosine_alpha_bar, cosine_posterior_variance)
