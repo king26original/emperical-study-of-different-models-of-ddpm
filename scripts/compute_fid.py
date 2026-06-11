afdba98766208b175a9bcfd40f7658b7f6574aa6
king26original <analogsns@gmail.com>
2026-06-11 18:42:14 +0530

    Create compute_fid.py

---
+++ scripts/compute_fid.py
@@ -0,0 +1,6 @@
+from src.evaluation.fid import fid, fid_fn
+
+fid_score_model1=fid_fn(model1, fid)
+fid_score_model2=fid_fn(model2, fid)
+fid_score_model3=fid_fn(model3, fid)
+fid_score_model4=fid_fn(model4, fid)
