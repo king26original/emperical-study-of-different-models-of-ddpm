4ba0571d6bf4aafd258ba5f86e31acf340e482ff
king26original <analogsns@gmail.com>
2026-06-11 18:37:51 +0530

    Create train_models.py

---
+++ scripts/train_models.py
@@ -0,0 +1,51 @@
+from src.model.multi_head_attention import model_multi
+from src.model.single_head_attention import model
+
+model1=model()
+model2=model()
+model3=model()
+model4=model_multi()
+
+ema_model3=copy.deepcopy(model3)
+ema_model4=copy.deepcopy(model4)
+
+from src.data.dataloader import trainloader
+from src.diffusion.linear_beta_schedule import linear_alpha_bar
+
+losses1=train(
+    model1,
+    trainloader,
+    alpha_bar=linear_alpha_bar,
+    'model1.pth',
+    ema_model_name=False,
+    ema_model=False
+)
+
+losses2=train(
+    model2,
+    trainloader,
+    alpha_bar=linear_alpha_bar,
+    'model2.pth',
+    ema_model_name=False,
+    ema_model=False
+)
+
+from src.diffusion.cosine_beta_schedule import cosine_alpha_bar
+
+losses3=train(
+    model3,
+    trainloader,
+    alpha_bar=cosine_alpha_bar,
+    ddpm_model_name=False,
+    ema_model_name='ema_model3.pth',
+    ema_model=ema_model3
+)
+
+losses4=train(
+    model4,
+    trainloader,
+    alpha_bar=cosine_alpha_bar,
+    ddpm_model_name=False,
+    ema_model_name='ema_model4.pth',
+    ema_model=ema_model4
+):
