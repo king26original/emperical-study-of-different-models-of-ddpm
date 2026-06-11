74aa0e3f215808a13ca9471c5ade7cd1d8dd8f87
king26original <analogsns@gmail.com>
2026-06-11 17:57:31 +0530

    cosine beta schedule

---
+++ src/diffusion/cosine beta schedule.py
@@ -0,0 +1,27 @@
+timesteps=1000
+
+s=0.008
+
+steps=timesteps+1
+
+x=torch.linspace(
+    0,
+    timesteps,
+    steps,
+)
+
+cosine_alpha_bar=torch.cos(((x/timesteps+s)/(1+s))*math.pi*0.5)**2
+
+cosine_alpha_bar=cosine_alpha_bar/cosine_alpha_bar[0]
+
+cosine_beta=1-(cosine_alpha_bar[1:]/cosine_alpha_bar[:-1])
+
+cosine_beta=torch.clamp(cosine_beta, 1e-4, 0.999)
+
+cosine_alpha=1.0-cosine_beta
+
+cosine_alpha_bar=torch.cumprod(cosine_alpha, dim=0)
+
+cosine_posterior_variance=cosine_beta.clone()
+
+cosine_posterior_variance[1:]=(cosine_beta[1:]*(1-cosine_alpha_bar[:-1])/(1-cosine_alpha_bar[1:]))
