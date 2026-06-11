6dba2abe386a383c2422ce93dc47aed9e7622023
king26original <analogsns@gmail.com>
2026-06-11 17:55:07 +0530

    defines ema

---
+++ src/diffusion/ema.py
@@ -0,0 +1,7 @@
+@torch.no_grad()
+def update_ema(ema_model, model, decay=0.995):
+  ema_params=dict(ema_model.named_parameters())
+  model_params=dict(model.named_parameters())
+
+  for k in ema_params.keys():
+    ema_params[k].data.mul_(decay).add_(model_params[k].data, alpha=1-decay)
