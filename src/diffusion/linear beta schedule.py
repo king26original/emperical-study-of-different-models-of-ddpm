23c8af0730765e86b3394122bdef729c225e1bbb
king26original <analogsns@gmail.com>
2026-06-11 17:56:31 +0530

    linear beta schedule

---
+++ src/diffusion/linear beta schedule.py
@@ -0,0 +1,15 @@
+timesteps = 1000
+
+linear_beta=torch.linspace(
+    1e-4,
+    0.02,
+    timesteps
+)
+
+linear_alpha=1.0-linear_beta
+
+linear_alpha_bar=torch.cumprod(linear_alpha, dim=0)
+
+linear_posterior_variance=linear_beta.clone()
+
+linear_posterior_variance[1:]=(linear_beta[1:]*(1-linear_alpha_bar[:-1])/(1-linear_alpha_bar[1:]))
