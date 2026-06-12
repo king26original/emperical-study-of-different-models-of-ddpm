463c3b6a62b8d68ac785aae02800584e2ce2c18c
king26original <analogsns@gmail.com>
2026-06-12 09:36:04 +0530

    Update train_models.py

--- scripts/train_models.py
+++ scripts/train_models.py
@@ -51,3 +51,8 @@ losses4=train(
     ema_model_name='ema_model4.pth',
     ema_model=ema_model4
 )
+
+torch.save(model1.state_dict(), 'model1.pth')
+torch.save(model2.state_dict(), 'model2.pth')
+torch.save(ema_model3.state_dict(), 'ema_model3.pth')
+torch.save(ema_model4.state_dict(), 'ema_model4.pth')
